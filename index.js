4146e715e217e7bf1e6fb000fc7d9dc40a0d3f2e
Bowen Zhong <bz4e14@soton.ac.uk>
2017-11-09 23:54:10 +0800

    server updaye

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const debug = require('ghost-ignition').debug('boot:index');
 let app = require('./core');
 
 app().start()
-  // .then(() => {
-  //   console.log(`App boot ${(Date.now() - startTime) / 1000} s.`);
-  // })
-  // .catch(() => {
-  //   console.log('Error happened when app boot');
-  //   process.exit(-1);
-  // })
+  .then(() => {
+    console.log(`App boot ${(Date.now() - startTime) / 1000} s.`);
+  })
+  .catch(() => {
+    console.log('Error happened when app boot');
+    process.exit(-1);
+  })
